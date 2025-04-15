9a76b22fd65f0cfd0e2464c13158f4c77ce8e38c
sebastian tia <sebastian.tht@gmail.com>
2025-04-15 12:04:13 -0700

    fix flag

--- kernel/src/table_configuration.rs
+++ kernel/src/table_configuration.rs
@@ -268,7 +268,7 @@ impl TableConfiguration {
     /// both the protocol's readerFeatures and writerFeatures.
     ///
     /// See: <https://github.com/delta-io/delta/blob/master/PROTOCOL.md#v2-checkpoint-table-feature>
-    #[cfg_attr(feature = "developer-visibility", visibility::make(pub))]
+    #[cfg_attr(feature = "internal-api", visibility::make(pub))]
     #[allow(unused)] // needed to compile w/o default features
     pub(crate) fn is_v2_checkpoint_supported(&self) -> bool {
         let read_supported = self
